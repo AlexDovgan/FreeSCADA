f499c7386d0baf93a223f8c61a0862348f9f0551
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2008-03-02 17:21:09 +0000

    Fix on previous commit ...

---
+++ trunk/FreeSCADA2/Common/SchemaModule/Manipulators/GeometryHilightManipulator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FreeSCADA.Schema.Manipulators
+{
+    class GeometryHilightManipulator:BaseManipulator
+    {
+        Rectangle hilightRect = new Rectangle();
+        public GeometryHilightManipulator(UIElement element, SchemaDocument doc)
+            : base(element, doc)
+        {
+            VisualBrush brush = new VisualBrush(AdornedElement);
+            //hilightRect.Opacity = 0.5;
+          //  hilightRect.Fill = brush;
+            
+            visualChildren.Add(hilightRect);
+        }
+        
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+            Rect r=AdornedElement.TransformToVisual(this).TransformBounds(new Rect(new Point(0, 0), AdornedElement.DesiredSize));
+            r.X = 0;
+            r.Y = 0;
+            hilightRect.Arrange(r);
+            return finalSize;
+        }
+          
+    }
+}
