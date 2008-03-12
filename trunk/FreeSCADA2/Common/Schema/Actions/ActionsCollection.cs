e964c45cea250d2d88dfa9b9284f2fab3f3a2e62
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2008-03-12 20:19:53 +0000

    Added missed files.

---
+++ trunk/FreeSCADA2/Common/Schema/Actions/ActionsCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.ComponentModel;
+
+namespace FreeSCADA.Common.Schema.Actions
+{
+    /// <summary>
+    /// Actions manager class 
+    /// </summary>
+    [ContentProperty("ActionsList")]
+    public class ActionsCollection:DependencyObject
+    {
+
+        List<BaseAction> actionsList = new List<BaseAction>();
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public List<BaseAction> ActionsList
+        {
+            get { return actionsList; }
+        }
+
+        public static readonly DependencyProperty Actions = DependencyProperty.RegisterAttached(
+            "Actions",
+            typeof(ActionsCollection),
+            typeof(ActionsCollection),
+            new FrameworkPropertyMetadata());
+
+
+        public static ActionsCollection GetActions(FrameworkElement el)
+        {
+            if(el.GetValue(Actions)==null)
+                el.SetValue(Actions, new ActionsCollection());
+            return (ActionsCollection)el.GetValue(Actions);
+        }
+        public static void  SetActions(FrameworkElement el,ActionsCollection ac)
+        {
+            el.SetValue(Actions,ac);
+        }
+
+     }
+}
---
+++ trunk/FreeSCADA2/Common/Schema/Actions/BaseAction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.ComponentModel;
+using FreeSCADA.ShellInterfaces;
+
+namespace FreeSCADA.Common.Schema.Actions
+{
+    /// <summary>
+    /// base action class 
+    /// </summary>
+    abstract public class BaseAction
+    {
+        protected string actionChannelName;
+        protected FrameworkElement actionedObject;
+        protected string objectName;
+        protected IChannel actionChannel;
+        protected bool isLinked=false;
+
+        public string ActionChannelName
+        {
+            get{  return actionChannelName; }
+            set{  actionChannelName = value; }
+        }
+
+        /*public string ActionedObjectName
+        {
+            get { return objectName; }
+            set {objectName=value;}
+        }
+        */
+
+         
+        public bool ActivateActionFor(FrameworkElement obj)
+        {
+            if ((actionChannel = Env.Current.CommunicationPlugins.GetChannel(ActionChannelName)) != null &&
+                CheckActionFor(obj))
+            {
+                actionedObject = obj;
+                isLinked = true;
+                PrepareExecute();
+                actionChannel.ValueChanged += Execute;
+                
+                return true;
+            }
+            return false;
+        }
+        public void DeactivateAction()
+        {
+            actionChannel.ValueChanged -= Execute;
+        }
+        
+        protected virtual void PrepareExecute()
+        {
+
+        }
+
+        protected virtual void Execute(object sender,EventArgs e)
+        {
+
+        }
+        public  virtual bool CheckActionFor(FrameworkElement obj)
+        {
+            return true;
+        }
+        
+    }
+}
