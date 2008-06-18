74210acfdf808b42fd6237677a6bcd28e8a64573
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2008-06-18 14:30:23 +0000

    Added missed files.

---
+++ trunk/FreeSCADA2/Common/Schema/ChannelsDataSource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+
+namespace FreeSCADA.Common.Schema
+{
+    class ChannelsDataSource: DataSourceProvider
+    {
+        string channelName;
+
+        /// <summary>
+        /// Gets/sets the path of a resource file 
+        /// from which to get text.
+        /// </summary>
+        public string ChannelName
+        {
+            get { return this.channelName; }
+            set
+            {
+                if (value == this.channelName)
+                    return;
+
+                this.channelName = value;
+
+                //base.OnPropertyChanged(new PropertyChangedEventArgs("ChannelName"));
+            }
+        }
+
+        protected override void BeginQuery()
+        {
+            string text = null;
+
+            // Load text out of the resource file.
+           
+            base.OnQueryFinished(text);
+        }
+
+    }
+}
