40134c5aefdb38400950dc7c8ee7dd8efe59a371
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2008-12-30 11:16:28 +0000

    added missed file.

---
+++ Common/Schema/CnanelValidator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace FreeSCADA.Common.Schema
+{
+    public class ChannelValidator : ValidationRule
+    {
+        private double _min;
+        private double _max;
+
+        public ChannelValidator()
+        {
+        }
+
+        public double Min
+        {
+            get { return _min; }
+            set { _min = value; }
+        }
+
+        public double Max
+        {
+            get { return _max; }
+            set { _max = value; }
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            double val = (Double)value;
+
+           
+            if ((val < Min) || (val > Max))
+            {
+                return new ValidationResult(false,
+                  "Channel's value out of range: " + Min + " - " + Max + ".");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+        
+    }
+
+
+}
