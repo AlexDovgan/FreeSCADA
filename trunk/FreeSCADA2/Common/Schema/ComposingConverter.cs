1fcdd5eca401098a87a05c6ae94e80270a60fed5
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2008-12-31 17:08:36 +0000

    Small symbolic New Year update. Fixed functionality with channel  range definition with bindings. From now we can declare ranges for channel value. Initial implementation of expression binding definition. Need in some improvements. Happy New Year guys! Good Luck in New Year and a I wish us lot of power and possibilities to bring our project for logical end. I believe that we can to make a great product which will be come a strong competitor for any commercial product on the market! Happy New Year!

---
+++ trunk/FreeSCADA2/Common/Schema/ComposingConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using  System.Windows.Markup;
+using System.ComponentModel;
+
+namespace FreeSCADA.Common.Schema
+{
+    [ContentProperty("Converters")]
+    public class ComposingConverter : IValueConverter
+    {
+        private List<IValueConverter> converters = new List<IValueConverter>();
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public List<IValueConverter> Converters
+        {
+            get { return this.converters; }
+        }
+
+        public object Convert(object o, Type targetType, object parameter, CultureInfo culture)
+        {
+            for (int i = 0; i < this.converters.Count; i++)
+            {
+                o = converters[i].Convert(o, targetType, parameter, culture);
+            }
+            return o;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            for (int i = this.converters.Count - 1; i >= 0; i--)
+            {
+                value = converters[i].ConvertBack(value, targetType, parameter, culture);
+            }
+            return value;
+        }
+    }
+}
+
