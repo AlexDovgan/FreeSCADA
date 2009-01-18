c169fc276ae1938ee4622b16b0d36ae9af4ff0d0
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2009-01-18 08:30:15 +0000

    Added missed file

---
+++ Designer/SchemaEditor/SelectionHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FreeSCADA.Designer.SchemaEditor
+{
+    class SelectionManager
+    {
+        Canvas mainCanvas;
+        protected List<UIElement> _selected=new List<UIElement>();
+        SelectionManager(Canvas canvas)
+        {
+            mainCanvas = canvas;
+            
+        }
+        public List<UIElement>  Selected
+        {
+            get {return _selected;}
+        }
+        public Rect GetSelectedBounds()
+        {
+            EditorHelper.CalculateBoundce(Selected, mainCanvas);
+
+            return new Rect();
+        }
+        public List<UIElement> Clone()
+        {
+            return new List<UIElement>();
+        }
+
+
+    }
+}
