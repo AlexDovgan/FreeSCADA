ca30e2e702810ec668005efcd87a6460c2ce10d7
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2009-02-16 14:37:17 +0000

    Color bindig is implemented as part of common binding editing dialog. Also need to implemented as property editor.

---
+++ trunk/FreeSCADA2/Common/Schema/SolidBrushConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace FreeSCADA.Common.Schema
+{
+    /// <summary>
+    /// Converter for brush bindung
+    /// SoldBrush
+    /// </summary>
+    public class SolidBrushConverter : IValueConverter
+    {
+        public Color StartColor
+        {
+            get;
+            set;
+        }
+        public Color EndColor
+        {
+            get;
+            set;
+        }
+        public Double MinValue
+        {
+            get;
+            set;
+        }
+        public Double MaxValue
+        {
+            get;
+            set;
+        }
+        public SolidBrushConverter()
+        {
+        }
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val;
+
+            if (Double.TryParse(value.ToString(), out val) && targetType == typeof(Brush))
+            {
+
+                Color color = new Color();
+                double curPos = (val - MinValue) / (MaxValue - MinValue);
+                color.A = (byte)(Math.Abs(StartColor.A - Math.Abs(EndColor.A - StartColor.A) * curPos));
+                color.R = (byte)(Math.Abs(StartColor.R - Math.Abs(EndColor.R - StartColor.R) * curPos));
+                color.G = (byte)(Math.Abs(StartColor.G - Math.Abs(EndColor.G - StartColor.G) * curPos));
+                color.B = (byte)(Math.Abs(StartColor.B - Math.Abs(EndColor.B - StartColor.B) * curPos));
+                return new SolidColorBrush(color);
+            }
+            return null;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new Exception("Can convert back");
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/FreeSCADA2/Designer/SchemaEditor/PropertiesUtils/SolidBrushBindingPanel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Data;
+using FreeSCADA.Common.Schema;
+using FreeSCADA.Interfaces;
+
+namespace FreeSCADA.Designer.SchemaEditor.PropertiesUtils
+{
+    internal partial class SolidBrushBindingPanel : BaseBindingPanel
+    {
+        IChannel channel;
+
+        public SolidBrushBindingPanel()
+        {
+            InitializeComponent();
+            label3.ForeColor = Color.Red;
+            label4.Text = "";
+        }
+
+        public override void AddChannel(IChannel channel)
+        {
+            if (channel != null)
+            {
+                this.channel = channel;
+
+                label3.ForeColor = Color.FromKnownColor(KnownColor.ControlText);
+                label4.Text = channel.Name;
+            }
+        }
+
+        public override void Initialize(object element, PropertyInfo property, System.Windows.Data.BindingBase binding)
+        {
+            base.Initialize(element, property, binding);
+
+            System.Windows.Data.Binding bind = binding as System.Windows.Data.Binding;
+            if (bind != null)
+            {
+                ChannelDataProvider cdp = (ChannelDataProvider)bind.Source;
+                AddChannel(cdp.Channel);
+                SolidBrushConverter sbc = bind.Converter as SolidBrushConverter;
+                minEdit.Value = (Decimal)sbc.MinValue;
+                maxEdit.Value = (Decimal)sbc.MaxValue;
+                startColorButton.BackColor = Color.FromArgb(
+                    sbc.StartColor.A,
+                    sbc.StartColor.R,
+                    sbc.StartColor.G,
+                    sbc.StartColor.B);
+                endColorButton.BackColor = Color.FromArgb(
+                    sbc.EndColor.A,
+                    sbc.EndColor.R,
+                    sbc.EndColor.G,
+                    sbc.EndColor.B);
+
+
+
+            }
+        }
+        public override System.Windows.Data.BindingBase Save()
+        {
+            if (channel != null)
+            {
+                System.Windows.Data.Binding bind = new System.Windows.Data.Binding("Value");
+                ChannelDataProvider cdp = new ChannelDataProvider();
+                cdp.ChannelName = channel.PluginId + "." + channel.Name;
+                bind.Source = cdp;
+                cdp.Refresh();
+
+                SolidBrushConverter conv = new SolidBrushConverter();
+                conv.MinValue = Decimal.ToDouble(minEdit.Value);
+                conv.MaxValue = Decimal.ToDouble(maxEdit.Value);
+                conv.StartColor = System.Windows.Media.Color.FromArgb(
+                    startColorButton.BackColor.A,
+                    startColorButton.BackColor.R,
+                    startColorButton.BackColor.G,
+                    startColorButton.BackColor.B);
+                conv.EndColor = System.Windows.Media.Color.FromArgb(
+                    endColorButton.BackColor.A,
+                    endColorButton.BackColor.R,
+                    endColorButton.BackColor.G,
+                    endColorButton.BackColor.B);
+
+                bind.Converter = conv;
+
+                bind.Mode = BindingMode.TwoWay;
+
+
+                DependencyObject depObj;
+                DependencyProperty depProp;
+                GetPropertyObjects(element, Property, out depObj, out depProp);
+                if (depObj != null && depProp != null)
+                    bind.FallbackValue = depObj.GetValue(depProp);
+
+                return bind;
+            }
+            else
+                return base.Save();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ColorDialog cd = new System.Windows.Forms.ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                (sender as System.Windows.Forms.Button).BackColor = cd.Color;
+
+            }
+        }
+
+
+    }
+
+    internal class BrushBindingPanelFactory : BaseBindingPanelFactory
+    {
+        override public bool CheckApplicability(object element, PropertyInfo property)
+        {
+            Type type = BaseBindingPanel.GetPropertyType(element, property);
+            if (type.Equals(typeof(System.Windows.Media.Brush)))
+                return true;
+
+            return false;
+        }
+
+        override public bool CanWorkWithBinding(System.Windows.Data.BindingBase binding)
+        {
+            if (binding != null && binding is System.Windows.Data.Binding)
+            {
+                System.Windows.Data.Binding bind = binding as System.Windows.Data.Binding;
+                if (bind.Source is ChannelDataProvider == false)
+                    return false;
+
+                if (bind.Converter is SolidBrushConverter == false)
+                    return false;
+
+                return true;
+            }
+
+            return true;
+        }
+
+        override public BaseBindingPanel CreateInstance()
+        {
+            return new SolidBrushBindingPanel();
+        }
+
+        override public string Name
+        {
+            get { return StringResources.SolidColorBindingPanelName; }
+        }
+    }
+}
