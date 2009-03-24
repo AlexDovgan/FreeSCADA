df3a36294ca538bd44648a3bfe9d71174ca77c7f
alex_dov <alex_dov@748b2178-a613-0410-8967-bf7732abb7ce>
2009-03-24 07:46:31 +0000

    Added missed file

---
+++ Common/Schema/Converters/ScriptConverter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using Microsoft.Scripting.Hosting;
+using IronPython.Hosting;
+
+namespace FreeSCADA.Common.Schema
+{
+    public class ScriptConverter : IMultiValueConverter
+    {
+        public string Expression
+        {
+            get;
+            set;
+        }
+        static ScriptEngine python = InitializePython();
+        public ScriptConverter()
+        {
+        }
+
+        public ScriptConverter(string exp)
+        {
+            Expression = exp;
+        }
+        #region IMultiValueConverter Members
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string pyExpr = "from math import *\n" + Expression;
+            ScriptSource source= python.CreateScriptSourceFromString(pyExpr, Microsoft.Scripting.SourceCodeKind.Statements);;
+            ScriptScope scope = python.CreateScope();
+            scope.SetVariable("values",values);
+            scope.SetVariable("res", 0.0);
+            scope.SetVariable("object", parameter);
+
+            if (source != null)
+            {
+                try
+                {
+                    source.Execute(scope);
+                }
+                catch (System.Exception e)
+                {
+                    Env.Current.Logger.LogWarning(string.Format("ScriptConverter: Failed to execute script converter on : {0}, Error: {1}", parameter, e.Message));
+                    return null;
+                }
+            }
+            return scope.GetVariable("res");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return value;
+        }
+        static ScriptEngine InitializePython()
+        {
+            Dictionary<string, object> options = new Dictionary<string, object>();
+            options["DivisionOptions"] = IronPython.PythonDivisionOptions.New;
+            return Python.CreateEngine(options);
+        }
+
+       
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
