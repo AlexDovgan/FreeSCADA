162ad7e7b1eee8a563c45e73f2b451b25b2dd832
michael_tutin <michael_tutin@748b2178-a613-0410-8967-bf7732abb7ce>
2009-08-11 07:05:53 +0000

    Adding missing file.

---
+++ trunk/FreeSCADA2/CLServer/ChannelState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FreeSCADA.CLServer
+{
+	public enum ChannelStatusFlags
+	{
+		Unknown = 0,
+		Good = 1,
+		Bad = 2,
+		NotUsed = 0xFFFF
+	}
+
+	[DataContract]
+	public class ChannelState
+	{
+		[DataMember]
+		public string Type { get; set; }
+
+		[DataMember]
+		public string Value { get; set; }
+
+		[DataMember]
+		public DateTime ModifyTime { get; set; }
+
+		[DataMember]
+		public ChannelStatusFlags Status { get; set; }
+	}
+}
